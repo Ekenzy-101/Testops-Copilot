9d414880d91e88b53e147e4b697d91e49fac9f9d
Emmanuel Onyekaba <ekeneonyekaba@gmail.com>
2025-12-14 18:56:38 +0300

    fix: add missing packages

--- frontend/rstest.config.ts
+++ frontend/rstest.config.ts
@@ -3,13 +3,13 @@ import rsbuildConfig from "./rsbuild.config";
 
 export default defineConfig({
   coverage: {
+    enabled: true,
     thresholds: {
       branches: 60,
       functions: 60,
       lines: 60,
       statements: 60,
     },
-    enabled: true,
   },
   globals: true,
   plugins: rsbuildConfig.plugins,
